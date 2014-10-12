13e11f17ddff5aa0db09e8c648f7a4dfdcfe558b
SeangJemmy <SeangJemmy@users.noreply.github.com>
2014-10-12 19:49:01 +0700

    Create Intant-Spleef-Arena-Mod.js

---
+++ Intant-Spleef-Arena-Mod.js
@@ -0,0 +1,631 @@
+//Instant Spleef Arena Mod by SeangJemmy®
+
+function useItem(x, y, z, itemid, blockId){
+if(itemId==265){
+
+setTile(x, y, z, 80);
+
+setTile(x-1, y, z, 80);
+setTile(x+1, y, z, 80);
+setTile(x, y, z+1, 80);
+setTile(x, y, z-1, 80);
+setTile(x-1, y, z-1, 80);
+setTile(x-1, y, z+1, 80);
+setTile(x+1, y, z-1, 80);
+setTile(x+1, y, z+1, 80);
+
+setTile(x-2, y, z, 80);
+setTile(x+2, y, z, 80);
+setTile(x, y, z+2, 80);
+setTile(x, y, z-2, 80);
+setTile(x-2, y, z-2, 80);
+setTile(x-2, y, z+2, 80);
+setTile(x+2, y, z-2, 80);
+setTile(x+2, y, z+2, 80);
+
+setTile(x-3, y, z, 80);
+setTile(x+3, y, z, 80);
+setTile(x, y, z+3, 80);
+setTile(x, y, z-3, 80);
+setTile(x-3, y, z-3, 80);
+setTile(x-3, y, z+3, 80);
+setTile(x+3, y, z-3, 80);
+setTile(x+3, y, z+3, 80);
+
+setTile(x-4, y, z, 80);
+setTile(x+4, y, z, 80);
+setTile(x, y, z+4, 80);
+setTile(x, y, z-4, 80);
+setTile(x-4, y, z-4, 80);
+setTile(x-4, y, z+4, 80);
+setTile(x+4, y, z-4, 80);
+setTile(x+4, y, z+4, 80);
+
+setTile(x-5, y, z, 80);
+setTile(x+5, y, z, 80);
+setTile(x, y, z+5, 80);
+setTile(x, y, z-5, 80);
+setTile(x-5, y, z-5, 80);
+setTile(x-5, y, z+5, 80);
+setTile(x+5, y, z-5, 80);
+setTile(x+5, y, z+5, 80);
+
+setTile(x-6, y, z, 80);
+setTile(x+6, y, z, 80);
+setTile(x, y, z+6, 80);
+setTile(x, y, z-6, 80);
+setTile(x-6, y, z-6, 80);
+setTile(x-6, y, z+6, 80);
+setTile(x+6, y, z-6, 80);
+setTile(x+6, y, z+6, 80);
+
+setTile(x-1, y, z, 80);
+setTile(x+1, y, z, 80);
+setTile(x, y, z+2, 80);
+setTile(x, y, z-2, 80);
+setTile(x-1, y, z-2, 80);
+setTile(x-1, y, z+2, 80);
+setTile(x+1, y, z-2, 80);
+setTile(x+1, y, z+2, 80);
+
+setTile(x-1, y, z, 80);
+setTile(x+1, y, z, 80);
+setTile(x, y, z+3, 80);
+setTile(x, y, z-3, 80);
+setTile(x-1, y, z-3, 80);
+setTile(x-1, y, z+3, 80);
+setTile(x+1, y, z-3, 80);
+setTile(x+1, y, z+3, 80);
+
+setTile(x-1, y, z, 80);
+setTile(x+1, y, z, 80);
+setTile(x, y, z+4, 80);
+setTile(x, y, z-4, 80);
+setTile(x-1, y, z-4, 80);
+setTile(x-1, y, z+4, 80);
+setTile(x+1, y, z-4, 80);
+setTile(x+1, y, z+4, 80);
+
+setTile(x-1, y, z, 80);
+setTile(x+1, y, z, 80);
+setTile(x, y, z+5, 80);
+setTile(x, y, z-5, 80);
+setTile(x-1, y, z-5, 80);
+setTile(x-1, y, z+5, 80);
+setTile(x+1, y, z-5, 80);
+setTile(x+1, y, z+5, 80);
+
+setTile(x-1, y, z, 80);
+setTile(x+1, y, z, 80);
+setTile(x, y, z+6, 80);
+setTile(x, y, z-6, 80);
+setTile(x-1, y, z-6, 80);
+setTile(x-1, y, z+6, 80);
+setTile(x+1, y, z-6, 80);
+setTile(x+1, y, z+6, 80);
+
+setTile(x-1, y, z, 80);
+setTile(x+1, y, z, 80);
+setTile(x, y, z+2, 80);
+setTile(x, y, z-2, 80);
+setTile(x-1, y, z-2, 80);
+setTile(x-1, y, z+2, 80);
+setTile(x+1, y, z-2, 80);
+setTile(x+1, y, z+2, 80);
+
+setTile(x-2, y, z, 80);
+setTile(x+2, y, z, 80);
+setTile(x, y, z+3, 80);
+setTile(x, y, z-3, 80);
+setTile(x-2, y, z-3, 80);
+setTile(x-2, y, z+3, 80);
+setTile(x+2, y, z-3, 80);
+setTile(x+2, y, z+3, 80);
+
+setTile(x-2, y, z, 80);
+setTile(x+2, y, z, 80);
+setTile(x, y, z+4, 80);
+setTile(x, y, z-4, 80);
+setTile(x-2, y, z-4, 80);
+setTile(x-2, y, z+4, 80);
+setTile(x+2, y, z-4, 80);
+setTile(x+2, y, z+4, 80);
+
+setTile(x-2, y, z, 80);
+setTile(x+2, y, z, 80);
+setTile(x, y, z+5, 80);
+setTile(x, y, z-5, 80);
+setTile(x-2, y, z-5, 80);
+setTile(x-2, y, z+5, 80);
+setTile(x+2, y, z-5, 80);
+setTile(x+2, y, z+5, 80);
+
+setTile(x-2, y, z, 80);
+setTile(x+2, y, z, 80);
+setTile(x, y, z+6, 80);
+setTile(x, y, z-6, 80);
+setTile(x-2, y, z-6, 80);
+setTile(x-2, y, z+6, 80);
+setTile(x+2, y, z-6, 80);
+setTile(x+2, y, z+6, 80);
+
+setTile(x-2, y, z, 80);
+setTile(x+2, y, z, 80);
+setTile(x, y, z+2, 80);
+setTile(x, y, z-2, 80);
+setTile(x-2, y, z-2, 80);
+setTile(x-2, y, z+2, 80);
+setTile(x+2, y, z-2, 80);
+setTile(x+2, y, z+2, 80);
+
+setTile(x-3, y, z, 80);
+setTile(x+3, y, z, 80);
+setTile(x, y, z+3, 80);
+setTile(x, y, z-3, 80);
+setTile(x-3, y, z-3, 80);
+setTile(x-3, y, z+3, 80);
+setTile(x+3, y, z-3, 80);
+setTile(x+3, y, z+3, 80);
+
+setTile(x-3, y, z, 80);
+setTile(x+3, y, z, 80);
+setTile(x, y, z+4, 80);
+setTile(x, y, z-4, 80);
+setTile(x-3, y, z-4, 80);
+setTile(x-3, y, z+4, 80);
+setTile(x+3, y, z-4, 80);
+setTile(x+3, y, z+4, 80);
+
+setTile(x-3, y, z, 80);
+setTile(x+3, y, z, 80);
+setTile(x, y, z+5, 80);
+setTile(x, y, z-5, 80);
+setTile(x-3, y, z-5, 80);
+setTile(x-3, y, z+5, 80);
+setTile(x+3, y, z-5, 80);
+setTile(x+3, y, z+5, 80);
+
+setTile(x-3, y, z, 80);
+setTile(x+3, y, z, 80);
+setTile(x, y, z+6, 80);
+setTile(x, y, z-6, 80);
+setTile(x-3, y, z-6, 80);
+setTile(x-3, y, z+6, 80);
+setTile(x+3, y, z-6, 80);
+setTile(x+3, y, z+6, 80);
+
+setTile(x-2, y, z, 80);
+setTile(x+2, y, z, 80);
+setTile(x, y, z+2, 80);
+setTile(x, y, z-2, 80);
+setTile(x-2, y, z-2, 80);
+setTile(x-2, y, z+2, 80);
+setTile(x+2, y, z-2, 80);
+setTile(x+2, y, z+2, 80);
+
+setTile(x-3, y, z, 80);
+setTile(x+3, y, z, 80);
+setTile(x, y, z+3, 80);
+setTile(x, y, z-3, 80);
+setTile(x-3, y, z-3, 80);
+setTile(x-3, y, z+3, 80);
+setTile(x+3, y, z-3, 80);
+setTile(x+3, y, z+3, 80);
+
+setTile(x-4, y, z, 80);
+setTile(x+4, y, z, 80);
+setTile(x, y, z+4, 80);
+setTile(x, y, z-4, 80);
+setTile(x-4, y, z-4, 80);
+setTile(x-4, y, z+4, 80);
+setTile(x+4, y, z-4, 80);
+setTile(x+4, y, z+4, 80);
+
+setTile(x-4, y, z, 80);
+setTile(x+4, y, z, 80);
+setTile(x, y, z+5, 80);
+setTile(x, y, z-5, 80);
+setTile(x-4, y, z-5, 80);
+setTile(x-4, y, z+5, 80);
+setTile(x+4, y, z-5, 80);
+setTile(x+4, y, z+5, 80);
+
+setTile(x-4, y, z, 80);
+setTile(x+4, y, z, 80);
+setTile(x, y, z+6, 80);
+setTile(x, y, z-6, 80);
+setTile(x-4, y, z-6, 80);
+setTile(x-4, y, z+6, 80);
+setTile(x+4, y, z-6, 80);
+setTile(x+4, y, z+6, 80);
+
+setTile(x-2, y, z, 80);
+setTile(x+2, y, z, 80);
+setTile(x, y, z+2, 80);
+setTile(x, y, z-2, 80);
+setTile(x-2, y, z-2, 80);
+setTile(x-2, y, z+2, 80);
+setTile(x+2, y, z-2, 80);
+setTile(x+2, y, z+2, 80);
+
+setTile(x-3, y, z, 80);
+setTile(x+3, y, z, 80);
+setTile(x, y, z+3, 80);
+setTile(x, y, z-3, 80);
+setTile(x-3, y, z-3, 80);
+setTile(x-3, y, z+3, 80);
+setTile(x+3, y, z-3, 80);
+setTile(x+3, y, z+3, 80);
+
+setTile(x-4, y, z, 80);
+setTile(x+4, y, z, 80);
+setTile(x, y, z+4, 80);
+setTile(x, y, z-4, 80);
+setTile(x-4, y, z-4, 80);
+setTile(x-4, y, z+4, 80);
+setTile(x+4, y, z-4, 80);
+setTile(x+4, y, z+4, 80);
+
+setTile(x-5, y, z, 80);
+setTile(x+5, y, z, 80);
+setTile(x, y, z+5, 80);
+setTile(x, y, z-5, 80);
+setTile(x-5, y, z-5, 80);
+setTile(x-5, y, z+5, 80);
+setTile(x+5, y, z-5, 80);
+setTile(x+5, y, z+5, 80);
+
+setTile(x-5, y, z, 80);
+setTile(x+5, y, z, 80);
+setTile(x, y, z+6, 80);
+setTile(x, y, z-6, 80);
+setTile(x-5, y, z-6, 80);
+setTile(x-5, y, z+6, 80);
+setTile(x+5, y, z-6, 80);
+setTile(x+5, y, z+6, 80);
+
+setTile(x-2, y, z, 80);
+setTile(x+2, y, z, 80);
+setTile(x, y, z+1, 80);
+setTile(x, y, z-1, 80);
+setTile(x-2, y, z-1, 80);
+setTile(x-2, y, z+1, 80);
+setTile(x+2, y, z-1, 80);
+setTile(x+2, y, z+1, 80);
+
+setTile(x-3, y, z, 80);
+setTile(x+3, y, z, 80);
+setTile(x, y, z+1, 80);
+setTile(x, y, z-1, 80);
+setTile(x-3, y, z-1, 80);
+setTile(x-3, y, z+1, 80);
+setTile(x+3, y, z-1, 80);
+setTile(x+3, y, z+1, 80);
+
+setTile(x-4, y, z, 80);
+setTile(x+4, y, z, 80);
+setTile(x, y, z+1, 80);
+setTile(x, y, z-1, 80);
+setTile(x-4, y, z-1, 80);
+setTile(x-4, y, z+1, 80);
+setTile(x+4, y, z-1, 80);
+setTile(x+4, y, z+1, 80);
+
+setTile(x-5, y, z, 80);
+setTile(x+5, y, z, 80);
+setTile(x, y, z+1, 80);
+setTile(x, y, z-1, 80);
+setTile(x-5, y, z-1, 80);
+setTile(x-5, y, z+1, 80);
+setTile(x+5, y, z-1, 80);
+setTile(x+5, y, z+1, 80);
+
+setTile(x-6, y, z, 80);
+setTile(x+6, y, z, 80);
+setTile(x, y, z+1, 80);
+setTile(x, y, z-1, 80);
+setTile(x-6, y, z-1, 80);
+setTile(x-6, y, z+1, 80);
+setTile(x+6, y, z-1, 80);
+setTile(x+6, y, z+1, 80);
+
+setTile(x-2, y, z, 80);
+setTile(x+2, y, z, 80);
+setTile(x, y, z+2, 80);
+setTile(x, y, z-2, 80);
+setTile(x-2, y, z-2, 80);
+setTile(x-2, y, z+2, 80);
+setTile(x+2, y, z-2, 80);
+setTile(x+2, y, z+2, 80);
+
+setTile(x-3, y, z, 80);
+setTile(x+3, y, z, 80);
+setTile(x, y, z+2, 80);
+setTile(x, y, z-2, 80);
+setTile(x-3, y, z-2, 80);
+setTile(x-3, y, z+2, 80);
+setTile(x+3, y, z-2, 80);
+setTile(x+3, y, z+2, 80);
+
+setTile(x-4, y, z, 80);
+setTile(x+4, y, z, 80);
+setTile(x, y, z+2, 80);
+setTile(x, y, z-2, 80);
+setTile(x-4, y, z-2, 80);
+setTile(x-4, y, z+2, 80);
+setTile(x+4, y, z-2, 80);
+setTile(x+4, y, z+2, 80);
+
+setTile(x-5, y, z, 80);
+setTile(x+5, y, z, 80);
+setTile(x, y, z+2, 80);
+setTile(x, y, z-2, 80);
+setTile(x-5, y, z-2, 80);
+setTile(x-5, y, z+2, 80);
+setTile(x+5, y, z-2, 80);
+setTile(x+5, y, z+2, 80);
+
+setTile(x-6, y, z, 80);
+setTile(x+6, y, z, 80);
+setTile(x, y, z+2, 80);
+setTile(x, y, z-2, 80);
+setTile(x-6, y, z-2, 80);
+setTile(x-6, y, z+2, 80);
+setTile(x+6, y, z-2, 80);
+setTile(x+6, y, z+2, 80);
+
+setTile(x-2, y, z, 80);
+setTile(x+2, y, z, 80);
+setTile(x, y, z+2, 80);
+setTile(x, y, z-2, 80);
+setTile(x-2, y, z-2, 80);
+setTile(x-2, y, z+2, 80);
+setTile(x+2, y, z-2, 80);
+setTile(x+2, y, z+2, 80);
+
+setTile(x-3, y, z, 80);
+setTile(x+3, y, z, 80);
+setTile(x, y, z+3, 80);
+setTile(x, y, z-3, 80);
+setTile(x-3, y, z-3, 80);
+setTile(x-3, y, z+3, 80);
+setTile(x+3, y, z-3, 80);
+setTile(x+3, y, z+3, 80);
+
+setTile(x-4, y, z, 80);
+setTile(x+4, y, z, 80);
+setTile(x, y, z+3, 80);
+setTile(x, y, z-3, 80);
+setTile(x-4, y, z-3, 80);
+setTile(x-4, y, z+3, 80);
+setTile(x+4, y, z-3, 80);
+setTile(x+4, y, z+3, 80);
+
+setTile(x-5, y, z, 80);
+setTile(x+5, y, z, 80);
+setTile(x, y, z+3, 80);
+setTile(x, y, z-3, 80);
+setTile(x-5, y, z-3, 80);
+setTile(x-5, y, z+3, 80);
+setTile(x+5, y, z-3, 80);
+setTile(x+5, y, z+3, 80);
+
+setTile(x-6, y, z, 80);
+setTile(x+6, y, z, 80);
+setTile(x, y, z+3, 80);
+setTile(x, y, z-3, 80);
+setTile(x-6, y, z-3, 80);
+setTile(x-6, y, z+3, 80);
+setTile(x+6, y, z-3, 80);
+setTile(x+6, y, z+3, 80);
+
+setTile(x-2, y, z, 80);
+setTile(x+2, y, z, 80);
+setTile(x, y, z+2, 80);
+setTile(x, y, z-2, 80);
+setTile(x-2, y, z-2, 80);
+setTile(x-2, y, z+2, 80);
+setTile(x+2, y, z-2, 80);
+setTile(x+2, y, z+2, 80);
+
+setTile(x-3, y, z, 80);
+setTile(x+3, y, z, 80);
+setTile(x, y, z+3, 80);
+setTile(x, y, z-3, 80);
+setTile(x-3, y, z-3, 80);
+setTile(x-3, y, z+3, 80);
+setTile(x+3, y, z-3, 80);
+setTile(x+3, y, z+3, 80);
+
+setTile(x-4, y, z, 80);
+setTile(x+4, y, z, 80);
+setTile(x, y, z+4, 80);
+setTile(x, y, z-4, 80);
+setTile(x-4, y, z-4, 80);
+setTile(x-4, y, z+4, 80);
+setTile(x+4, y, z-4, 80);
+setTile(x+4, y, z+4, 80);
+
+setTile(x-5, y, z, 80);
+setTile(x+5, y, z, 80);
+setTile(x, y, z+4, 80);
+setTile(x, y, z-4, 80);
+setTile(x-5, y, z-4, 80);
+setTile(x-5, y, z+4, 80);
+setTile(x+5, y, z-4, 80);
+setTile(x+5, y, z+4, 80);
+
+setTile(x-6, y, z, 80);
+setTile(x+6, y, z, 80);
+setTile(x, y, z+4, 80);
+setTile(x, y, z-4, 80);
+setTile(x-6, y, z-4, 80);
+setTile(x-6, y, z+4, 80);
+setTile(x+6, y, z-4, 80);
+setTile(x+6, y, z+4, 80);
+
+setTile(x-2, y, z, 80);
+setTile(x+2, y, z, 80);
+setTile(x, y, z+2, 80);
+setTile(x, y, z-2, 80);
+setTile(x-2, y, z-2, 80);
+setTile(x-2, y, z+2, 80);
+setTile(x+2, y, z-2, 80);
+setTile(x+2, y, z+2, 80);
+
+setTile(x-3, y, z, 80);
+setTile(x+3, y, z, 80);
+setTile(x, y, z+3, 80);
+setTile(x, y, z-3, 80);
+setTile(x-3, y, z-3, 80);
+setTile(x-3, y, z+3, 80);
+setTile(x+3, y, z-3, 80);
+setTile(x+3, y, z+3, 80);
+
+setTile(x-4, y, z, 80);
+setTile(x+4, y, z, 80);
+setTile(x, y, z+4, 80);
+setTile(x, y, z-4, 80);
+setTile(x-4, y, z-4, 80);
+setTile(x-4, y, z+4, 80);
+setTile(x+4, y, z-4, 80);
+setTile(x+4, y, z+4, 80);
+
+setTile(x-5, y, z, 80);
+setTile(x+5, y, z, 80);
+setTile(x, y, z+5, 80);
+setTile(x, y, z-5, 80);
+setTile(x-5, y, z-5, 80);
+setTile(x-5, y, z+5, 80);
+setTile(x+5, y, z-5, 80);
+setTile(x+5, y, z+5, 80);
+
+setTile(x-6, y, z, 80);
+setTile(x+6, y, z, 80);
+setTile(x, y, z+5, 80);
+setTile(x, y, z-5, 80);
+setTile(x-6, y, z-5, 80);
+setTile(x-6, y, z+5, 80);
+setTile(x+6, y, z-5, 80);
+setTile(x+6, y, z+5, 80);
+
+setTile(x-7, y, z, 80);//wall blocks;
+setTile(x+7, y, z, 80);
+setTile(x, y, z+7, 80);
+setTile(x, y, z-7, 80);
+setTile(x-7, y, z-7, 80);
+setTile(x-7, y, z+7, 80);
+setTile(x+7, y, z-7, 80);
+setTile(x+7, y, z+7, 80);
+
+setTile(x-7, y, z, 80);
+setTile(x+7, y, z, 80);
+setTile(x, y, z+1, 80);
+setTile(x, y, z-1, 80);
+setTile(x-7, y, z-1, 80);
+setTile(x-7, y, z+1, 80);
+setTile(x+7, y, z-1, 80);
+setTile(x+7, y, z+1, 80);
+
+setTile(x-7, y, z, 80);
+setTile(x+7, y, z, 80);
+setTile(x, y, z+2, 80);
+setTile(x, y, z-2, 80);
+setTile(x-7, y, z-2, 80);
+setTile(x-7, y, z+2, 80);
+setTile(x+7, y, z-2, 80);
+setTile(x+7, y, z+2, 80);
+
+setTile(x-7, y, z, 80);
+setTile(x+7, y, z, 80);
+setTile(x, y, z+3, 80);
+setTile(x, y, z-3, 80);
+setTile(x-7, y, z-3, 80);
+setTile(x-7, y, z+3, 80);
+setTile(x+7, y, z-3, 80);
+setTile(x+7, y, z+3, 80);
+
+setTile(x-7, y, z, 80);
+setTile(x+7, y, z, 80);
+setTile(x, y, z+4, 80);
+setTile(x, y, z-4, 80);
+setTile(x-7, y, z-4, 80);
+setTile(x-7, y, z+4, 80);
+setTile(x+7, y, z-4, 80);
+setTile(x+7, y, z+4, 80);
+
+setTile(x-7, y, z, 80);
+setTile(x+7, y, z, 80);
+setTile(x, y, z+5, 80);
+setTile(x, y, z-5, 80);
+setTile(x-7, y, z-5, 80);
+setTile(x-7, y, z+5, 80);
+setTile(x+7, y, z-5, 80);
+setTile(x+7, y, z+5, 80);
+
+setTile(x-7, y, z, 80);
+setTile(x+7, y, z, 80);
+setTile(x, y, z+6, 80);
+setTile(x, y, z-6, 80);
+setTile(x-7, y, z-6, 80);
+setTile(x-7, y, z+6, 80);
+setTile(x+7, y, z-6, 80);
+setTile(x+7, y, z+6, 80);
+
+
+setTile(x-1, y, z, 80);
+setTile(x+1, y, z, 80);
+setTile(x, y, z+7, 80);
+setTile(x, y, z-7, 80);
+setTile(x-1, y, z-7, 80);
+setTile(x-1, y, z+7, 80);
+setTile(x+1, y, z-7, 80);
+setTile(x+1, y, z+7, 80);
+
+setTile(x-2, y, z, 80);
+setTile(x+2, y, z, 80);
+setTile(x, y, z+7, 80);
+setTile(x, y, z-7, 80);
+setTile(x-2, y, z-7, 80);
+setTile(x-2, y, z+7, 80);
+setTile(x+2, y, z-7, 80);
+setTile(x+2, y, z+7, 80);
+
+setTile(x-3, y, z, 80);
+setTile(x+3, y, z, 80);
+setTile(x, y, z+7, 80);
+setTile(x, y, z-7, 80);
+setTile(x-3, y, z-7, 80);
+setTile(x-3, y, z+7, 80);
+setTile(x+3, y, z-7, 80);
+setTile(x+3, y, z+7, 80);
+
+setTile(x-4, y, z, 80);
+setTile(x+4, y, z, 80);
+setTile(x, y, z+7, 80);
+setTile(x, y, z-7, 80);
+setTile(x-4, y, z-7, 80);
+setTile(x-4, y, z+7, 80);
+setTile(x+4, y, z-7, 80);
+setTile(x+4, y, z+7, 80);
+
+setTile(x-5, y, z, 80);
+setTile(x+5, y, z, 80);
+setTile(x, y, z+7, 80);
+setTile(x, y, z-7, 80);
+setTile(x-5, y, z-7, 80);
+setTile(x-5, y, z+7, 80);
+setTile(x+5, y, z-7, 80);
+setTile(x+5, y, z+7, 80);
+
+setTile(x-6, y, z, 80);
+setTile(x+6, y, z, 80);
+setTile(x, y, z+7, 80);
+setTile(x, y, z-7, 80);
+setTile(x-6, y, z-7, 80);
+setTile(x-6, y, z+7, 80);
+setTile(x+6, y, z-7, 80);
+setTile(x+6, y, z+7, 80);
+
+}
+}
